67734d390eb52125133d9e0dc927f99230f71ee4
Aji Hamdani Ahmad <85418881+Danni4421@users.noreply.github.com>
2023-07-03 21:38:40 +0700

    data validation implemented
    implementing data validation hapi with joi

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-const Hapi = require("@hapi/hapi");
-const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
@@ -19,6 +20,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
